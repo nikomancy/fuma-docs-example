ff23ff61da8e2319df53cd142779c5247f0bc194
Niko <39569270+nikomancy@users.noreply.github.com>
2024-11-05 13:33:07 -0700

    Updating front page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">Veeva Technical Test</h1>
       <p className="text-fd-muted-foreground">
         You can open{' '}
         <Link
@@ -12,7 +12,7 @@ export default function HomePage() {
         >
           /docs
         </Link>{' '}
-        and see the documentation.
+        to see the documentation for this technical test.
       </p>
     </main>
   );
