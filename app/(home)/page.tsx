684449e8d63991ece1b47f31c83d1e9d1c5bd3a2
Niko <39569270+nikomancy@users.noreply.github.com>
2024-11-05 13:35:53 -0700

    Signing my name on it.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Veeva Technical Test</h1>
+      <h1 className="mb-4 text-2xl font-bold">Niko Berry - Veeva Technical Test</h1>
       <p className="text-fd-muted-foreground">
         You can open{' '}
         <Link
